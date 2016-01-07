db01c91006cdf414bb2d9478acae00e6a5ae437b
aagaidiu <alexey.a.gaidiukov@intel.com>
2016-01-07 13:07:26 -0800

    Graph dfs, loop detection

--- p505/d.cpp
+++ p505/d.cpp
@@ -1,37 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 10005;
-vector<int> d[N];
-bool vis[N], checked[N];
-int n, m, a, b;
+const int N = 100005;
+vector<int> v[N], u[N], w;
+int c[N]; // color 0:white 1:gray 2:black
+int d[N]; // discovered
+int n, m, a, b, ans;
+
+int dfs(int i) {
+    c[i]++; // grey
+    for (auto k:v[i]) {
+        if (c[k] == 1) return 1; // back edge -> cycle
+        if (c[k] == 0 && dfs(k)) return 1;
+    }
+    c[i]++; // black
+    return 0;
+}
+
 int main() {
     scanf("%d%d", &n, &m);
     for (int i=0; i<m; i++) {
         scanf("%d%d", &a, &b);
-        d[a].push_back(b);
+        v[a].push_back(b); // Directed
+        u[a].push_back(b); // Undirected
+        u[b].push_back(a);
     }
-    int nodes = 0;
-    // count number of nodes
-    for (int i=1; i<=n; i++)
-        nodes += d[a].size() > 0;
 
-    // Check if there are any cycles
-    bool no_cycles = true;
-    for (int i=1; i<=n && no_cycles; i++) {
-        if (checked[i]) continue;
-        memset(vis, 0, sizeof(vis));
-        queue<int> q;
-        q.push(i);
-        printf("--i=%d\n", i);
+    for (int i=0; i<=n; i++) {
+        if (d[i] || !u[i].size()) continue;
+        d[i]++;
+        // BFS to explore elements in one disjoint set
+        w.clear();
+        deque<int> q;
+        q.push_back(i);
         while(!q.empty()) {
-            int j = q.front(); q.pop();
-            printf("j=%d vis[j]=%d\n", j, vis[j]);
-            checked[j] = true;
-            if (vis[j]) { no_cycles = false; break; }
-            vis[j] = true;
-            for (auto c:d[j]) q.push(c);
+            int j = q.front(); q.pop_front();
+            w.push_back(j);
+            for (auto k:u[j]) if (!d[k]) {
+                q.push_back(k);
+                d[k]++;
+                ans++;
+            }
+        }
+        // Add one if there's a cycle
+        for (auto l:w) {
+            if (c[l]) continue;
+            if (dfs(l)) {
+                ans++;
+                break;
+            }
         }
     }
-    printf("nodes=%d no_cycles=%d\n", nodes, no_cycles);
-    printf("%d\n", nodes - no_cycles);
+
+    printf("%d\n", ans);
 }
